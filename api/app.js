f6052a59bbcdb3498b1cbd81c095f8d62a7f8a8c
Sstark97 <aitorscinfo@gmail.com>
2023-02-20 20:13:32 +0000

    Add CORS

--- api/app.js
+++ api/app.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import cors from "cors"
 import { useGraphql } from './graphql/graphql.js';
 
 const createApp = async () => {
   const app = express();
 
   app.use(express.json());
+  app.use(cors({
+    origin: '*'
+  }))
 
   app.get('/', (req, res) => {
     res.send('Hola mi server en express');
