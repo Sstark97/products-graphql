a187baef8c6fa349ae3924b696e61211fb42534b
Sstark97 <aitorscinfo@gmail.com>
2023-02-20 13:04:07 +0000

    Add resolvers folder

--- api/graphql/resolvers/product.resolvers.js
+++ api/graphql/resolvers/product.resolvers.js
@@ -1,23 +1,53 @@
 const getProduct = (_, { id }) => {
-    return {
-       id,
-       name: "Producto 1",
-       price: 3000.2,
-       description: "bla bla bla",
-       image: "http://image.png",
-       createdAt: new Date().toISOString()
-   }
-}
+  return {
+    id,
+    name: "Producto 1",
+    price: 3000.2,
+    description: "bla bla bla",
+    image: "http://image.png",
+    createdAt: new Date().toISOString(),
+  };
+};
 
 const getProducts = () => {
-   return []
-}
+  return [
+   {
+      id: 1,
+      name: "Producto 1",
+      price: 3000.2,
+      description: "bla bla bla",
+      image: "http://image.png",
+      createdAt: new Date().toISOString(),
+   },
+   {
+      id: 2,
+      name: "Producto 2",
+      price: 3000.2,
+      description: "bla bla bla",
+      image: "http://image.png",
+      createdAt: new Date().toISOString(),
+   },
+   {
+      id: 3,
+      name: "Producto 3",
+      price: 3000.2,
+      description: "bla bla bla",
+      image: "http://image.png",
+      createdAt: new Date().toISOString(),
+   },
+   {
+      id: 4,
+      name: "Producto 4",
+      price: 3000.2,
+      description: "bla bla bla",
+      image: "http://image.png",
+      createdAt: new Date().toISOString(),
+   }
+  ];
+};
 
 const addProduct = () => {
-   // codigo
-}
+  // codigo
+};
 
-export { 
-    getProduct, 
-    getProducts 
-}
+export { getProduct, getProducts };
--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 import { createApp } from "./api/app.js";
 
-// (async () => {
-//   const port = process.env.PORT || 3000;
-//   const app = await createApp();
-  
-//   app.listen(port, () => {
-//     console.log(`Mi port ${port}`);
-//   });
-// })();
-
 const port = process.env.PORT || 3000;
 const app = await createApp();
 
